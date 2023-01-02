a9ba2e845f3deaa522b5b9fafa21de9936f541de
viba98 <32447352+viba98@users.noreply.github.com>
2023-01-02 22:23:15 +0530

    everything

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,13 @@ import { withBrowser } from "./browser"
  * the deploy script.
  */
 export default async function main(req: Request, res: Response) {
-	console.log("1")
+	console.log("1st test")
 	await withBrowser(async browser => {
-		console.log("2")
-		await browser.visit("https://www.notion.so")
-		console.log("3")
+		console.log("2nd test")
+		await browser.visit("https://vectormagic.com/")
+		console.log("3rd test")
 		// Wait for the product page to load.
-		await browser.findText("All-in-one workspace")
+		await browser.findText("Easily Convert")
 		console.log("4")
 		const source = await browser.driver.getPageSource()
 		console.log("5")
